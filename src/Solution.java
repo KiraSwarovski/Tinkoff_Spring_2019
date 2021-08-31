1292b37ae49b9e50073227ea5ea14560daf7df92
KiraSwarovski <jhosh1212@gmail.com>
2021-08-31 17:02:54 +0700

    First commit from IDE

---
+++ src/Solution.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = "  I  don't  know.";
+
+
+        int timesSpace = 0;
+        String res = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if(c == ' '){
+                timesSpace++;
+                if(timesSpace < 2)
+                    res += c;
+            }else{
+                res += c;
+                timesSpace = 0;
+            }
+        }
+
+        System.out.println(res.trim());
+       // System.out.println(s.replaceAll("( +)"," ").trim());
+    }
+}
